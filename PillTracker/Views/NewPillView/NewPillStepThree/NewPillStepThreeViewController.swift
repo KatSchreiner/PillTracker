b945186c418acb1d3d0d88ca1b821bf8197aec7f
Katerina Schreiner <kat.schreiner@yandex.ru>
2025-06-02 23:47:38 +0500

    Добавила в NewPillStepThreeViewController возможность установить продолжительность лечения

--- PillTracker/Views/NewPillView/NewPillStepThree/NewPillStepThreeViewController.swift
+++ PillTracker/Views/NewPillView/NewPillStepThree/NewPillStepThreeViewController.swift
@@ -16,7 +16,7 @@ class NewPillStepThreeViewController: UIViewController {
     // MARK: - Private Properties
     private lazy var repeatLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        label.font = UIFont.systemFont(ofSize: 18, weight: .regular)
         label.textColor = .dGray
         label.text = "Повторить"
         label.textAlignment = .left
@@ -38,7 +38,7 @@ class NewPillStepThreeViewController: UIViewController {
             button.backgroundColor = .lGray
             button.setTitle(day, for: .normal)
             button.setTitleColor(.gray, for: .normal)
-            button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
             button.tag = index
             button.addTarget(self, action: #selector(didTapDayButton), for: .touchUpInside)
             dayButtons.append(button)
@@ -48,9 +48,78 @@ class NewPillStepThreeViewController: UIViewController {
         return dayButtonStackView
     }()
     
+    private lazy var durationLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .dGray
+        label.text = "Продолжительность лечения"
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var durationSegmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["1 неделя", "2 недели", "1 месяц", "Другое"])
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addTarget(self, action: #selector(didChangeDuration), for: .valueChanged)
+        return segmentedControl
+    }()
+    
+    private lazy var startDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Начало лечения:"
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private lazy var startDatePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.locale = Locale(identifier: "ru_RU")
+        datePicker.tintColor = .dBlue
+        return datePicker
+    }()
+    
+    private lazy var endDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Окончание лечения:"
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private lazy var endDatePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.locale = Locale(identifier: "ru_RU")
+        datePicker.tintColor = .dBlue
+        return datePicker
+    }()
+    
+    private lazy var startDateStackView: UIStackView = {
+        let startDateStackView = UIStackView(arrangedSubviews: [startDateLabel, startDatePicker])
+        startDateStackView.axis = .horizontal
+        startDateStackView.spacing = 10
+        return startDateStackView
+    }()
+    
+    private lazy var endDateStackView: UIStackView = {
+        let endDateStackView = UIStackView(arrangedSubviews: [endDateLabel, endDatePicker])
+        endDateStackView.axis = .horizontal
+        endDateStackView.spacing = 10
+        return endDateStackView
+    }()
+    
+    private lazy var customDateRangeStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [startDateStackView, endDateStackView])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
+    
     private lazy var reminderLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        label.font = UIFont.systemFont(ofSize: 18, weight: .regular)
         label.textColor = .dGray
         label.text = "Напомнить?"
         label.textAlignment = .left
@@ -90,14 +159,33 @@ class NewPillStepThreeViewController: UIViewController {
         if model.selectedDays.contains(index) {
             model.selectedDays.remove(index)
             sender.backgroundColor = .lGray
-            sender.setTitleColor(.gray, for: .normal)
+            sender.setTitleColor(.dGray, for: .normal)
         } else {
             model.selectedDays.insert(index)
             sender.backgroundColor = .dBlue
             sender.setTitleColor(.lGray, for: .normal)
         }
-        print("Selected Days: \(model.selectedDays)")
+        
         updateNextButtonStateStepThree()
+    }
+    
+    @objc
+    private func didChangeDuration(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 3 {
+            if customDateRangeStackView.superview == nil {
+                
+                self.view.addSubview(customDateRangeStackView)
+                customDateRangeStackView.translatesAutoresizingMaskIntoConstraints = false
+                
+                NSLayoutConstraint.activate([
+                    customDateRangeStackView.topAnchor.constraint(equalTo: durationSegmentedControl.bottomAnchor, constant: 20),
+                    customDateRangeStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    customDateRangeStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                ])
+            }
+        } else {
+            customDateRangeStackView.removeFromSuperview()
+        }
     }
     
     @objc
@@ -111,7 +199,7 @@ class NewPillStepThreeViewController: UIViewController {
     private func setupView() {
         view.backgroundColor = .white
         
-        [repeatLabel, dayButtonStackView, reminderStackView].forEach { view in
+        [repeatLabel, dayButtonStackView, durationLabel, durationSegmentedControl,  reminderStackView].forEach { view in
             self.view.addSubview(view)
             view.translatesAutoresizingMaskIntoConstraints = false
         }
@@ -127,9 +215,18 @@ class NewPillStepThreeViewController: UIViewController {
             dayButtonStackView.topAnchor.constraint(equalTo: repeatLabel.bottomAnchor, constant: 20),
             dayButtonStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             dayButtonStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            dayButtonStackView.heightAnchor.constraint(equalToConstant: 40),
+            dayButtonStackView.heightAnchor.constraint(equalToConstant: 35),
             
-            reminderStackView.topAnchor.constraint(equalTo: dayButtonStackView.bottomAnchor, constant: 30),
+            
+            durationLabel.topAnchor.constraint(equalTo: dayButtonStackView.bottomAnchor, constant: 30),
+            durationLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            
+            durationSegmentedControl.topAnchor.constraint(equalTo: durationLabel.bottomAnchor, constant: 10),
+            durationSegmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            durationSegmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            durationSegmentedControl.heightAnchor.constraint(equalToConstant: 35),
+            
+            reminderStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
             reminderStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }

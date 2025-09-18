a17f87075c53343297e539a664ee41b1b82c3bc0
Katerina Schreiner <kat.schreiner@yandex.ru>
2025-09-18 12:04:58 +0300

    Создание кнопки вынесла в отдельный метод Констрейнты вынесла в отдельный метод Добавила MARKи

---
+++ PillTracker/Core/Custom/CustomButton.swift
@@ -0,0 +1,29 @@
+//
+//  MakeButton.swift
+//  PillTracker
+//
+//  Created by Екатерина Шрайнер on 18.09.2025.
+//
+
+import UIKit
+
+final class CustomButton {
+    static func makeButton(
+        title: String,
+        titleColor: UIColor,
+        backgroundColor: UIColor,
+        cornerRadius: CGFloat = 10,
+        contentEdgeInsets: UIEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20),
+        target: Any?,
+        action: Selector
+    ) -> UIButton {
+        let button = UIButton(type: .custom)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(titleColor, for: .normal)
+        button.backgroundColor = backgroundColor
+        button.layer.cornerRadius = cornerRadius
+        button.addTarget(target, action: action, for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }
+}
--- PillTracker/Views/MyPillView/DeleteAlertViewController.swift
+++ PillTracker/Views/MyPillView/DeleteAlertViewController.swift
@@ -8,97 +8,95 @@
 import UIKit
 
 final class DeleteAlertViewController: UIViewController {
+    //MARK: - Public properties
     var titleText: String?
-    var tableView: UITableView?
     
-    var onDeleteSingleDose: (() -> Void)?
-    var onDeleteFutureDoses: (() -> Void)?
-    var onCancel: (() -> Void)?
-
+    //MARK: - Private Properties
     private lazy var titleLabel: UILabel = {
         let titleLabel = UILabel()
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 18)
-        
+        titleLabel.font = Constants.titleFontSize
         titleLabel.textAlignment = .center
         return titleLabel
     }()
-
-    private lazy var deleteSingleDoseButton: UIButton = {
-        let deleteSingleDoseButton = UIButton(type: .custom)
-        deleteSingleDoseButton.setTitle("Удалить эту дозу", for: .normal)
-        deleteSingleDoseButton.setTitleColor(.dGray, for: .normal)
-        deleteSingleDoseButton.backgroundColor = .lGray
-        deleteSingleDoseButton.layer.cornerRadius = 8
-        deleteSingleDoseButton.addTarget(self, action: #selector(deleteSingleDoseTapped), for: .touchUpInside)
-        deleteSingleDoseButton.translatesAutoresizingMaskIntoConstraints = false
-        return deleteSingleDoseButton
-    }()
     
-    private lazy var deleteFutureDosesButton: UIButton = {
-        let deleteFutureDosesButton = UIButton(type: .custom)
-        deleteFutureDosesButton.setTitle("Удалить все дозы", for: .normal)
-        deleteFutureDosesButton.setTitleColor(.dGray, for: .normal)
-        deleteFutureDosesButton.backgroundColor = .lGray
-        deleteFutureDosesButton.layer.cornerRadius = 8
-        deleteFutureDosesButton.addTarget(self, action: #selector(deleteFutureDosesTapped), for: .touchUpInside)
-        deleteFutureDosesButton.translatesAutoresizingMaskIntoConstraints = false
-        return deleteFutureDosesButton
-    }()
+    private lazy var deleteSingleDoseButton = CustomButton.makeButton(
+        title: "Удалить одну дозу",
+        titleColor: .dGray,
+        backgroundColor: .lGray,
+        cornerRadius: Constants.defaultRadius,
+        target: self,
+        action: #selector(deleteSingleDoseTapped)
+    )
     
-    private lazy var cancelButton: UIButton = {
-        let cancelButton = UIButton(type: .custom)
-        cancelButton.setTitle("Отмена", for: .normal)
-        cancelButton.setTitleColor(.white, for: .normal)
-        cancelButton.backgroundColor = .lBlue
-        cancelButton.layer.cornerRadius = 8
-        cancelButton.addTarget(self, action: #selector(cancelTapped), for: .touchUpInside)
-        cancelButton.translatesAutoresizingMaskIntoConstraints = false
-        return cancelButton
-    }()
+    private lazy var deleteFutureDosesButton = CustomButton.makeButton(
+        title: "Удалить все будущие дозы",
+        titleColor: .dGray,
+        backgroundColor: .lGray,
+        cornerRadius: Constants.defaultRadius,
+        target: self,
+        action: #selector(deleteFutureDosesTapped)
+    )
+    
+    private lazy var cancelButton = CustomButton.makeButton(
+        title: "Отмена",
+        titleColor: .white,
+        backgroundColor: .lBlue,
+        cornerRadius: Constants.defaultRadius,
+        target: self,
+        action: #selector(cancelTapped)
+    )
     
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [titleLabel, deleteSingleDoseButton, deleteFutureDosesButton, cancelButton])
         stackView.axis = .vertical
-        stackView.spacing = 10
+        stackView.spacing = Constants.smallPadding
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
+    //MARK: - Callbacks
+    var onDeleteSingleDose: (() -> Void)?
+    var onDeleteFutureDoses: (() -> Void)?
+    var onCancel: (() -> Void)?
+    
+    //MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
     
+    //MARK: - IB Actions
     @objc private func deleteSingleDoseTapped() {
         deleteSingleDoseButton.animatePress()
-        
         onDeleteSingleDose?()
         dismiss(animated: true, completion: nil)
     }
     
     @objc private func deleteFutureDosesTapped() {
         deleteFutureDosesButton.animatePress()
-        
         onDeleteFutureDoses?()
         dismiss(animated: true, completion: nil)
     }
     
     @objc private func cancelTapped() {
         cancelButton.animatePress()
-        
         onCancel?()
         dismiss(animated: true, completion: nil)
     }
     
+    //MARK: Private Methods
     private func setupView() {
         view.backgroundColor = .white
         view.layer.cornerRadius = 15
         view.layer.masksToBounds = true
         
         titleLabel.text = titleText
-
-        view.addSubview(stackView)
         
+        view.addSubview(stackView)
+        addConstraints()
+    }
+    
+    private func addConstraints() {
         NSLayoutConstraint.activate([
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
